bb4936d418238de97adba3eab56e370889f4f144
Nur Yudha <nur.ramadhan@muf.co.id>
2024-07-22 15:52:24 +0700

    DEV -

--- src/main/java/com/java/elk/services/KakfaConsumerService.java
+++ src/main/java/com/java/elk/services/KakfaConsumerService.java
@@ -3,12 +3,42 @@ package com.java.elk.services;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 @Service
 public class KakfaConsumerService {
 
     @KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "${spring.kafka.consumer.group-id}")
     public void listen(String message) {
         System.out.println("Received message: " + message);
+
+        // Kirim data ke Logstash
+        String logstashUrl = "http://localhost:4445";
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(logstashUrl);
+            httpPost.setHeader("Content-Type", "application/json");
+            httpPost.setEntity(new StringEntity(message));
+
+            CloseableHttpResponse response = httpClient.execute(httpPost);
+            try {
+                HttpEntity responseEntity = response.getEntity();
+                String responseBody = EntityUtils.toString(responseEntity);
+                System.out.println("Response from Logstash: " + responseBody);
+            } finally {
+                response.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
